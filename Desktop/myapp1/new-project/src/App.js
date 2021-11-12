c390313e2c2d1ccdb50aba416b401f8d12ca1424
abdelkader ben gadha <kader131993@gmail.com>
2021-11-12 10:49:51 +0100

    first commit

---
+++ Desktop/myapp1/new-project/src/App.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Formu from './components/Formu';
+
+import './App.css'
+
+
+
+function App() {
+  return (
+    <div className="App">
+      
+   <h1>Contact</h1>
+   <div className="formul">
+   <Formu/>
+   </div>
+    </div>
+  );
+}
+
+export default App;
